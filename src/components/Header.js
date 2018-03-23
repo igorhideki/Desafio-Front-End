5911d2c2495cd10bbb4d0e4592b68ba318d8a117
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-23 17:40:07 -0400

    Adiciona icones no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Header = () => (
   <div className="header">
+    <span className="header__icon-menu"><i className="fas fa-bars fa-fw"></i></span>
     <span className="header__title">Comunicados</span>
+    <span className="header__icon-search"><i className="fas fa-search fa-fw"></i></span>    
   </div>
 );
 
