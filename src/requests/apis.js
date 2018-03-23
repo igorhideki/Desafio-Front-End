ad3a2fa4ffaba4f18497c6aec9831b7558531599
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-23 07:03:28 -0400

    Adiciona api get Comunicados

--- src/requests/apis.js
+++ src/requests/apis.js
@@ -5,7 +5,8 @@ const api = axios.create({
   baseURL: base
 });
 const apis = {
-  loadCategorias: () => api.get('tiposComunicados')
+  loadCategorias: () => api.get('tiposComunicados'),
+  loadComunicados: () => api.get('comunicados')
 };
 
 export default apis;
