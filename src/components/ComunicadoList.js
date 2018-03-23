27492784e72143ac56b37a1224d997d2dd4890a4
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-23 07:04:14 -0400

    Adiciona ComunicadoList

---
+++ src/components/ComunicadoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ComunicadoItem from './ComunicadoItem';
+
+const ComunicadoList = (props) => (
+  <ul>
+    {
+      props.comunicados && props.comunicados.map((comunicado) => (
+        <ComunicadoItem key={comunicado.id} {...comunicado}/>
+      ))
+    }
+  </ul>
+);
+
+ComunicadoList.propTypes = {
+  comunicados: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    seen: PropTypes.bool.isRequired,
+    type: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    fullDescription: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    file: PropTypes.shape({
+      type: PropTypes.string,
+      name: PropTypes.string,
+      url: PropTypes.string,
+    })
+  }))
+};
+
+export default ComunicadoList;
