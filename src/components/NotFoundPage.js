dc9b1364aa05a4d2cfa60a7e43cd18da1e976f75
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-24 17:01:24 -0400

    Adiciona container a NotFoundPage

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-  <p>404 - <Link to="/">Voltar para início</Link></p>
+  <div className="container">
+    <p>404 - <Link to="/">Voltar para início</Link></p>
+  </div>
 );
 
 export default NotFoundPage;
