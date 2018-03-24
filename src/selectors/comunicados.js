11bf0d2156f1b5cf01e2cd19e8da03105ab4a383
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-24 17:04:45 -0400

    Adiciona ordenação por data na lista de comunicados

--- src/selectors/comunicados.js
+++ src/selectors/comunicados.js
@@ -1,10 +1,16 @@
+import moment from 'moment';
+
 const getVisiblesComunicados = (comunicados, { type = -1 }) => {
+  const formatDate = 'YYYY-MM-DD HH:mm';
   return comunicados.filter((comunicado) => {
     if (type === -1) {
       return comunicado;
     }
     return comunicado.type === type;
+  }).sort((a, b) => {
+    return moment(a.date, formatDate).format() < moment(b.date, formatDate).format() ? 1 : -1;
   });
 };
 
 export default getVisiblesComunicados;
+
