856af454a1f57e84c6316e5f38606d4ed0c65a66
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-24 09:03:51 -0400

    Adiciona rotas para ComunicadoDetailsPage e NotFoundPage

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import ComunicadosPage from '../components/ComunicadosPage';
+import ComunicadoDetailsPage from '../components/ComunicadoDetailsPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -9,6 +11,8 @@ const AppRouter = () => (
       <Header />
       <Switch>
         <Route path="/" component={ComunicadosPage} exact />
+        <Route path="/details/:id" component={ComunicadoDetailsPage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </div>
   </BrowserRouter>
