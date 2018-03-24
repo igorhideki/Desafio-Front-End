044cf83177c223dee53ed2f0728fa5e8fd678862
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-24 17:00:40 -0400

    Adiciona AboutPage

---
+++ src/components/AboutPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div className="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam adipisci perferendis asperiores doloribus nisi, dicta ut velit? Quasi ex et sequi fuga dolores temporibus. Illum consequatur eligendi obcaecati expedita inventore minus aliquid sunt ipsum quibusdam. Unde porro fugiat ipsa. Nesciunt, magnam error. Error optio laborum quasi nulla temporibus tempora explicabo facilis minima doloremque? Consectetur tempore adipisci esse ea assumenda corporis voluptate tenetur velit reprehenderit debitis, sit in voluptates commodi asperiores inventore nisi itaque vero necessitatibus voluptatum beatae dolorum excepturi. Sint animi dolores optio consectetur dignissimos laudantium numquam tempore quos saepe recusandae? Nulla deserunt qui earum cupiditate facilis porro aspernatur iste.</p>
+  </div>
+);
+
+export default AboutPage;
