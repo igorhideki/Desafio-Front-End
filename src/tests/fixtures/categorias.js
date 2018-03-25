f2a2e6f7cc6e2ead6455c039e4cac2d5087b6c89
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-25 14:39:30 -0400

    Adiciona dados de categorias para testar os components

---
+++ src/tests/fixtures/categorias.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    type: 0,
+    name: 'Informativos SUN'
+  },
+  {
+    type: 1,
+    name: 'Força de Vendas'
+  },
+  {
+    type: 2,
+    name: 'Vivo Ligado'
+  },
+  {
+    type: 3,
+    name: 'Turbine Suas Vendas'
+  }
+];
