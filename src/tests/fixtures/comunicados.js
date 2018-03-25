37729aee0bc5592574cb4ae02e9f092394d813db
Igor Hideki Trindade <igorhideki@gmail.com>
2018-03-25 12:31:56 -0400

    Adiciona dados de comunicados para testar os components

---
+++ src/tests/fixtures/comunicados.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    title: 'Metas Março',
+    seen: true,
+    'type': 0,
+    'description': 'Planos controle agora também disponível no cartão de crédito.',
+    'fullDescription': 'Planos controle agora também disponível no cartão de crédito.',
+    'date': '2018-05-03 8:30'
+  },
+  {
+    id: 2,
+    title: 'Bônus Fevereiro',
+    seen: false,
+    type: 1,
+    description: 'Planos controle 5GB de internet 4G.',
+    fullDescription: 'Ligações ilimitadas locais para todas as operadoras. Ligações ilimitadas pra fixos nacional e qualquer Vivo do Brasil com o código 15. SMS ilimitado para qualquer operadora do Brasil.',
+    date: '2018-05-02 10:30'
+  },
+  {
+    id: 3,
+    title: 'Novos Serviços',
+    seen: false,
+    type: 1,
+    description: 'Novos serviços, clique para visualizar o material de divulgação.',
+    fullDescription: 'Novos serviços, clique para visualizar o material de divulgação.',
+    date: '2018-05-02 18:30',
+    files: [
+      {
+        type: 'image/*',
+        name: 'Material de divulgação',
+        url: 'logob4.png'
+      }
+    ]
+  }
+];
